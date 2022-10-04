f49c11c053554298ea05f662dd1fb51a4397ab6d
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-04 13:21:43 +0300

    Creaye header components

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import MobileNav from './MobileNav'
+import NavBar from './NavBar'
+import SocialMeidal from './SocialMeidal'
+
+function Header() {
+    const [background, setBg] = useState(false)
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            return window.scrollY > 50 ? setBg(true) : setBg(false)
+        })
+    }, [])
+    return (
+        <header className={`${background ? 'bg-dark h-20' : 'h-24 text-dark'} flex item-center fixed top-0 w-full text-white z-10 transition-all duration-300`}>
+            <div className="container mx-auto h-full flex item-center justify-between">
+    
+                {/* logo Image */}
+                {/* <NavBar /> */}
+                <div className=" lg:block">
+                    <NavBar /> 
+                    </div>
+                {/* <SocialMeidal/> */}
+                <div className=" lg:block">
+                    <SocialMeidal />
+                     </div>
+                {/* <MobileNav /> */}
+                <div className=" lg:block">
+                    <MobileNav />
+                    </div>
+
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
---
+++ src/components/SocialMeidal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SocialMeidal() {
+  return (
+    <div>SocialMeidal</div>
+  )
+}
+
+export default SocialMeidal
