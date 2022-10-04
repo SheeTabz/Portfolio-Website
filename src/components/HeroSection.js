ab38e93ef989c7a524308d9c5bb876972e6a7881
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-04 14:11:45 +0300

    Add herosection

---
+++ src/components/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <section className='lg:h-[85vh]  flex items-center bg-dark lg:bg-cover lg:gb-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden '>
+        <div className='container mx-auto ' >Hero </div>
+    </section>
+  )
+}
+
+export default HeroSection
