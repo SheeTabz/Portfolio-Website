861004822cabab8fe876f99a29051d853d0d29d1
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 18:14:12 +0300

    Craet project data

--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,71 @@
-const skills = [
+import RealEstate from '../assets/skills/estate.jpg'
+export const projectNavBar = [
     {
-        name: 'Figama',
-        image: ''
+        name: 'All'
+    },
+    {
+        name: 'Web Development',
+    },
+    {
+        name: 'UI/UX Design',
     }
+]
+export const projectInfo =[
+    {
+        id: 1,
+        name: 'Web Development',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    },
+    {
+        id: 2,
+        name: 'Web Development',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    },
+    {
+        id: 3,
+        name: 'Web Development',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    },
+    {
+        id: 4,
+        name: 'UI/UX Design',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    },
+    {
+        id: 5,
+        name: 'UI/UX Design',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    },
+    {
+        id: 6,
+        name: 'Web Development',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    },
+    {
+        id: 7,
+        name: 'Web Development',
+        title: 'Real Estate',
+        image: RealEstate ,
+        description: 'This is a real estate website ',
+        
+    }
+
 ]
