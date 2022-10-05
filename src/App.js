bbaf9decd5efaf4624f24b4cc07c18b0ce1f6c98
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 18:17:20 +0300

    Render component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import Portfolio from './components/Portfolio';
+import Services from './components/Services';
 import Skill from './components/Skill';
 import Skills from './components/Skills';
 
@@ -15,6 +18,9 @@ function App() {
     <Skill/>
     <About/>
     <Skills/>
+    <Services/>
+    <Portfolio/>
+    <Contact/>
     <div className="" style={{height: '2000px'}}></div>
    </div>
   );
