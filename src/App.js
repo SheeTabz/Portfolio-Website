ac0019d464d69a066b8edf33980dcb5807a5301c
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 10:03:46 +0300

    Add skill component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import './App.css';
+import About from './components/About';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import Skill from './components/Skill';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
    <div className=" ">
     <Header/>
     <HeroSection/>
+    <Skill/>
+    <About/>
+    <Skills/>
     <div className="" style={{height: '2000px'}}></div>
    </div>
   );
