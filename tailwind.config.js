8745420d52f7719fa564594947f3491719198bc0
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 10:07:29 +0300

    Change theme configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
         primary : "#312544",
         btncolor : "#A162CF",
         text : "#FFFFFF",
-        dark :  "#2A2253",
+        dark :  "#000000",
         text2 : "#8B85A9",
-        bright : "#8165D2 "
+        bright : "#C44CB6 ",
+        gradient1: '#CE6D88',
+        cardColor: '#191919'
       }
     },
     screens :{
