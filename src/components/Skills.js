45064bdb8b5e6a8632461da7476adc5297c191d1
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 10:06:33 +0300

    Create skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <section className='bg-cardColor py-12'>
+      <div className='container mx-auto'>
+        <div>
+          <img src='src/assets/images/tailwind.png'/>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
