4e83ad7e3a400b950ac52b5fbb8f11a2159032cb
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 10:06:20 +0300

    Creates skills section

---
+++ src/components/Skill.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Skill() {
+  return (
+    <section className='min-h-[146px] bg-cardColor flex items-center text-white'>
+        <div className='container mx-auto flex md:justfy-between items-center flex-wrap justify-evenly'>
+            <h1>Add something here Do not Forget</h1>
+        </div>
+
+    </section>
+  )
+}
+
+export default Skill
