acd05f905766d652c9da63a9c0c937b8a2181d1d
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 18:16:40 +0300

    Change them to be displayed

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header() {
         })
     }, [])
     return (
-        <header className={`${background ? 'bg-dark h-20 ' : 'h-24 text-dark'} flex item-center fixed  text-white top-0 w-full pt-8 z-10 transition-all duration-300`}>
+        <header className={`${background ? 'bg-dark h-20 ' : 'h-24'} flex item-center fixed text-white top-0 w-full pt-8 z-10 transition-all duration-300`}>
             <div className="container mx-auto h-full flex item-center justify-between">
             
                 {/* logo Image */}
