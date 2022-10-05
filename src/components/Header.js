a68999f34a8867330de01cae655bb56adf625f75
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-05 10:05:09 +0300

    Add social media section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,16 @@ import SocialMeidal from './SocialMeidal'
 
 
 function Header() {
+
     const [background, setBg] = useState(false)
+
     useEffect(() => {
         window.addEventListener("scroll", () => {
             return window.scrollY > 50 ? setBg(true) : setBg(false)
         })
     }, [])
     return (
-        <header className={`${background ? 'bg-dark h-20' : 'h-24 text-dark'} flex item-center fixed top-0 w-full text-white z-10 transition-all duration-300`}>
+        <header className={`${background ? 'bg-dark h-20 ' : 'h-24 text-dark'} flex item-center fixed  text-white top-0 w-full pt-8 z-10 transition-all duration-300`}>
             <div className="container mx-auto h-full flex item-center justify-between">
             
                 {/* logo Image */}
@@ -22,11 +24,11 @@ function Header() {
                     <NavBar /> 
                     </div>
                 {/* <SocialMeidal/> */}
-                {/* <div className=" hidden lg:block">
+                <div className=" lg:block hidden ">
                     <SocialMeidal />
-                     </div> */}
+                     </div>
                 {/* <MobileNav /> */}
-                <div className=" lg:block">
+                <div className=" hidden sm:block">
                     <MobileNav />
                     </div>
 
