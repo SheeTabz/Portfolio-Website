6e7e9fe1fb136e61abd344149cd69a687cb7fa75
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-06 23:23:40 +0300

    Import react packages

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { skills} from '../data'
+
+function Skills() {
+  
+  return (
+    <section id='skills'  className='bg-cardColor py-12 text-white'>
+      <div className='container mx-auto'>
+        <h2 className='text-center font-extrabold text-2xl'>My Skills & Proeficiencies</h2>
+        <div className='grid grid-cols-6 md:grid-flow-row gap-10 mt-8'>
+       
+          {skills.map(skill => {
+            return (< div className='imageSkill flex items-center justify-center'>
+                   <img src={skill.image} alt={skill.name} />
+                     
+            </div>)
+          })}
+
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
